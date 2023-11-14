1473515f43f058b4e00870c790ec8bca965cd80d
Yurnero <132830415+Bl4ckFury@users.noreply.github.com>
2023-11-14 03:21:59 +0300

    This is my third commit.
    This is my third commit.

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
+const menuBtn = document.querySelector(".nav-under__btn");
+const menuClose = document.querySelector(".btn-close");
+const menuList = document.querySelector(".under-links");
+
+menuBtn.addEventListener("click", () => {
+      menuList.classList.add("show-btn__menu");
+});
+
+menuClose.addEventListener("click", () => {
+      menuList.classList.remove("show-btn__menu");
+});
+
 let swiper = new Swiper(".swiper-slider", {
-  slidesPerView: 3.6,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-  navigation: {
-    nextEl: ".navigation-arrow__next",
-    prevEl: ".navigation-arrow__back",
-  },
-  // breakpoints: {
-  //   1470: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 10,
-  //   },
-  //   1024: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 10,
-  //   },
-  // },
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+      },
+      navigation: {
+            nextEl: ".navigation-arrow__next",
+            prevEl: ".navigation-arrow__back",
+      },
+      breakpoints: {
+            600: {
+                  slidesPerView: 1.4,
+                  spaceBetween: 20,
+            },
+            1470: {
+                  slidesPerView: 2.7,
+                  spaceBetween: 20,
+            },
+            1660: {
+                  slidesPerView: 3.7,
+                  spaceBetween: 20,
+            },
+      },
 });
 
 initMap();
 
 async function initMap() {
-  // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-  await ymaps3.ready;
-
-  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
-
-  // Иницилиазируем карту
-  const map = new YMap(
-    // Передаём ссылку на HTMLElement контейнера
-    document.getElementById("map"),
-
-    // Передаём параметры инициализации карты
-    {
-      location: {
-        // Координаты центра карты
-        center: [54.5293, 36.2754],
-
-        // Уровень масштабирования
-        zoom: 8,
-      },
-    }
-  );
-  // Добавляем слой для отображения схематической карты
-  map.addChild(new YMapDefaultSchemeLayer());
+      await ymaps3.ready;
+      const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+      const map = new YMap(document.getElementById("map"), {
+            location: {
+                  center: [54.54256107012005, 36.31914749999998],
+                  zoom: 17,
+            },
+      });
 }
+map.addChild(new YMapDefaultSchemeLayer());
 
-map.controls.remove("geolocationControl"); // удаляем геолокацию
-map.controls.remove("searchControl"); // удаляем поиск
-map.controls.remove("trafficControl"); // удаляем контроль трафика
-map.controls.remove("typeSelector"); // удаляем тип
-map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
-map.controls.remove("zoomControl"); // удаляем контрол зуммирования
-map.controls.remove("rulerControl"); // удаляем контрол правил
-// map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+map.controls.remove("geolocationControl");
+map.controls.remove("searchControl");
+map.controls.remove("trafficControl");
+map.controls.remove("typeSelector");
+map.controls.remove("fullscreenControl");
+map.controls.remove("zoomControl");
+map.controls.remove("rulerControl");
